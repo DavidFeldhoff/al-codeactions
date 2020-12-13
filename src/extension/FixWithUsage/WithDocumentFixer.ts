c60e9ccc0e32cb4380f544fca6c5ae82860a5e03
David Feldhoff <David.Feldhoff@gws.ms>
2020-12-13 15:17:47 +0100

    Remove command "Implicit with usage"

--- src/extension/FixWithUsage/WithDocumentFixer.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-export interface WithDocumentFixer {
-    addDocument(uri: vscode.Uri): void;
-    fixWithUsagesOfAllDocuments(): Promise<void>;
-    getNoOfUsagesFixed(): number;
-    getNoOfDocsFixed(): number;
-}
