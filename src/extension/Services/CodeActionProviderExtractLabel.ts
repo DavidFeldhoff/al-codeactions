bd9fac7e4da32680b027b97b767fb442a525009a
David Roys <David.Roys@intergen.co.nz>
2021-06-06 08:44:42 +1200

    First working version

--- src/extension/Services/CodeActionProviderExtractLabel.ts
+++ src/extension/Services/CodeActionProviderExtractLabel.ts
@@ -31,7 +31,21 @@ export class CodeActionProviderExtractLabel implements ICodeActionProvider {
 
         let stringLiteralRange: Range = DocumentUtils.trimRange(this.document, TextRangeExt.createVSCodeRange(this.stringLiteralTreeNode.fullSpan));
 
-        let variable: ALVariable = new ALVariable('newLabel', 'Label ' + this.document.getText(stringLiteralRange))
+        let placeHolderArray = [...new Set(this.document.getText(stringLiteralRange).match(/%[0-9]/g))]
+        let commentText: string = ''
+        if (placeHolderArray.length > 0) {
+            commentText = ', comment=\''
+
+            placeHolderArray.forEach((placeHolder: string, indexNumber: number) => {
+                if (indexNumber > 0)
+                    commentText += ', '
+                commentText += placeHolder + '= '
+            })
+
+            commentText += '\''
+        }
+
+        let variable: ALVariable = new ALVariable('newLabel', 'Label ' + this.document.getText(stringLiteralRange) + commentText) //todo - add comment
         let textEdit: TextEdit = WorkspaceEditUtils.addVariableToLocalVarSection(methodOrTriggerTreeNode, variable, this.document);
 
         let edit: WorkspaceEdit = new WorkspaceEdit();
